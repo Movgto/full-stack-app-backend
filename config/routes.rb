8a6f68d2076fc76c42e16b530ce06b4278376789
Cristian Villa <c.villavirgen@gmail.com>
2023-12-14 00:27:34 -0600

    Update routes to include items and reservations and custom routes for user-specific actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  # We could include these later when using rswag for API documentation
+  # mount Rswag::Ui::Engine => '/api-docs'
+  # mount Rswag::Api::Engine => '/api-docs'
+
   namespace :api do
     namespace :v1 do
+      # Custom routes for user-specific actions
+      get 'users/:user_id/reservations', to: 'reservations#user_reservations'
+      get 'users/:user_id/items', to: 'reservations#user_items'
+
+      resources :items
       resources :users
-      # Additional resources will go here
+      resources :reservations
     end
   end
-
-  # Keep the health check route as it is.
+  # Let's keep the health check route as it is.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # We might also want to define at some point a root route for our app.
