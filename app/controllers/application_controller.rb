b5d95dbe43456dc66e0f80b822e8408e042461e4
Cristian Villa <c.villavirgen@gmail.com>
2023-12-19 00:06:29 -0600

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-    before_action :set_default_url_options
-  
-    private
-  
-    def set_default_url_options
-      Rails.application.routes.default_url_options[:host] = 'localhost:3000'
-    end
+  before_action :set_default_url_options
+
+  private
+
+  def set_default_url_options
+    Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   end
-  
+end
