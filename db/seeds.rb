0e83dce683094da2ee6d7caae9a956fa109648bd
Cristian Villa <c.villavirgen@gmail.com>
2023-12-18 22:39:51 -0600

    Add ItemSerializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+require 'open-uri'
 
-user1 = User.create(username: 'user1')
+user1 = User.create!(username: 'user1')
 
-item1 = Item.create(
+item1 = Item.new(
   name: 'SuperSport 950',
-  image: 'https://images.ctfassets.net/x7j9qwvpvr5s/20GjWos8lkeYyKeLlUIkCl/e498d0e4568c1ccd95ace8cb918d1679/SS-950-MY22-Model-Preview-1050x650-v06.png',
   description: 'The SuperSport 950, with its dynamic style and compact dimensions, embodies the elegance of its design and the typical proportions of Ducatis Panigale sports motorcycles. In its new Stripe Livery color scheme, the SuperSport 950 showcases its most sophisticated essence and boldest character, representing Ducatis take on a sports motorcycle designed for the road.',
   deposit: 3000,
   finance_fee: 150,
@@ -16,3 +12,9 @@ item1 = Item.create(
   duration: 36,
   removed: false
 )
+
+# Download the image and attach it to the item
+image = URI.open('https://images.ctfassets.net/x7j9qwvpvr5s/20GjWos8lkeYyKeLlUIkCl/e498d0e4568c1ccd95ace8cb918d1679/SS-950-MY22-Model-Preview-1050x650-v06.png')
+item1.image.attach(io: image, filename: 'SS-950-MY22-Model-Preview-1050x650-v06.png')
+
+item1.save!
