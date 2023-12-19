e2bd1e87a9a033d9465fc9243f2cc5ab114646cb
Cristian Villa <c.villavirgen@gmail.com>
2023-12-18 23:13:29 -0600

    Add example motorcycle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
-class ApplicationController < ActionController::API
-end
+class ApplicationController < ActionController::Base
+    before_action :set_default_url_options
+  
+    private
+  
+    def set_default_url_options
+      Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+    end
+  end
+  
